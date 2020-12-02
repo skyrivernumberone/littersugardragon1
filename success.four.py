6638fca781dc0d4783ee3a599d97bf678336d3a8
Unknown <unknown@example.com>
2020-12-02 20:05:12 +0800

    selecting love me

---
+++ success.four.py
@@ -0,0 +1,11 @@
+x=0
+while x==0 :
+    a=int(input("请选这是否爱我"))
+    if a==1:
+        print('you are right')
+        x=1
+    elif a==2:
+        print('you will go die')
+
+    else:
+        print('you are a f1oolish')
